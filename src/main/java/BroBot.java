def8cace4758a45873f723b5a8ea1601de9c97f2
Matthew Kan (NUS Student) <e0795274@u.nus.edu>
2025-08-14 17:15:21 +0800

    Finished "Level 2. Add, List".

--- src/main/java/BroBot.java
+++ src/main/java/BroBot.java
@@ -1,8 +1,48 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.function.Supplier;
 
 public final class BroBot {
+    private static final class Task implements Comparable<Task> {
+        private final String taskName;
+        private final int id;
+        private final String logMessage;
+
+        private static int nextFreeID = 1;
+
+        private Task (final String taskName) {
+            this.taskName = taskName;
+            this.id = Task.nextFreeID;
+
+            this.logMessage = String.format("%d. %s", this.id, this.taskName);
+
+            Task.nextFreeID++;
+        }
+
+        @Override
+        public boolean equals (final Object obj) {
+            return this == obj;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.id;
+        }
+
+        @Override
+        public String toString() {
+            return this.logMessage;
+        }
+
+        @Override
+        public int compareTo (final Task otherTask) {
+            return this.id - otherTask.id;
+        }
+    }
+
     public static final String chatBotName = "BroBot";
+    private static final ArrayList<Task> taskList = new ArrayList<>();
+
     private static void greet() {
         BroBot.delimit();
         System.out.printf("Hello, I'm %s! What can I do for you?\n", BroBot.chatBotName);
@@ -27,8 +67,18 @@ public final class BroBot {
             return false;
         }
 
+        if (command.equals("list")) {
+            BroBot.delimit();
+            for (final Task task : taskList) {
+                System.out.println(task);
+            }
+            BroBot.delimit();
+            return true;
+        }
+
         BroBot.delimit();
-        System.out.println(command);
+        BroBot.taskList.add(new Task(command));
+        System.out.printf("added: %s\n", command);
         BroBot.delimit();
         return true;
     }
