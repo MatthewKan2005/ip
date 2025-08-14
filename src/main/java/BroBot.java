733c1e5b62323d21c1297c3b78c0c142cc5735ed
Matthew Kan (NUS Student) <e0795274@u.nus.edu>
2025-08-14 15:58:30 +0800

    Finished "Level 1. Echo".

--- src/main/java/BroBot.java
+++ src/main/java/BroBot.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
+import java.util.function.Supplier;
+
 public final class BroBot {
     public static final String chatBotName = "BroBot";
     private static void greet() {
-        System.out.printf("Hello, I'm %s! What can I do for you?", BroBot.chatBotName);
+        BroBot.delimit();
+        System.out.printf("Hello, I'm %s! What can I do for you?\n", BroBot.chatBotName);
+        BroBot.delimit();
+    }
+
+    private static void delimit() {
+        System.out.println("____________________________________________________________");
+    }
+
+    private static boolean processCommand() {
+        final Supplier<String> commandRetriever = () -> {
+            final String command = (new Scanner(System.in)).nextLine();
+            return command;
+        };
+
+        final String command = commandRetriever.get();
+        if (command.equals("bye")) {
+            BroBot.delimit();
+            System.out.println("Bye. Hope to see you again soon!");
+            BroBot.delimit();
+            return false;
+        }
+
+        BroBot.delimit();
+        System.out.println(command);
+        BroBot.delimit();
+        return true;
     }
 
     public static void main (final String[] args) {
         BroBot.greet();
+        while (BroBot.processCommand()) {
+
+        }
     }
 }
