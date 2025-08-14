887c57fc2608e37650efd6314c6949ba12eb5a56
Matthew Kan (NUS Student) <e0795274@u.nus.edu>
2025-08-14 20:14:00 +0800

    Finished "Level 3. Mark as Done".

--- src/main/java/BroBot.java
+++ src/main/java/BroBot.java
@@ -1,22 +1,36 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.function.Supplier;
+import java.util.regex.Pattern;
 
 public final class BroBot {
+    private BroBot() {
+
+    }
+
     private static final class Task implements Comparable<Task> {
         private final String taskName;
         private final int id;
-        private final String logMessage;
+        private String logMessage = null;
 
         private static int nextFreeID = 1;
+
+        private boolean isDone = false;
 
         private Task (final String taskName) {
             this.taskName = taskName;
             this.id = Task.nextFreeID;
-
-            this.logMessage = String.format("%d. %s", this.id, this.taskName);
-
             Task.nextFreeID++;
+        }
+
+        private void mark() {
+            this.isDone = true;
+            this.logMessage = null;
+        }
+
+        private void unmark() {
+            this.isDone = false;
+            this.logMessage = null;
         }
 
         @Override
@@ -31,6 +45,10 @@ public final class BroBot {
 
         @Override
         public String toString() {
+            if (this.logMessage == null) {
+                this.logMessage = String.format("%d. [%c] %s", this.id, (this.isDone ? 'X' : ' '), this.taskName);
+            }
+
             return this.logMessage;
         }
 
@@ -53,6 +71,8 @@ public final class BroBot {
         System.out.println("____________________________________________________________");
     }
 
+    private static final Pattern markCommandPattern = Pattern.compile("^mark [1-9]\\d*$");
+    private static final Pattern unmarkCommandPattern = Pattern.compile("^unmark [1-9]\\d*$");
     private static boolean processCommand() {
         final Supplier<String> commandRetriever = () -> {
             final String command = (new Scanner(System.in)).nextLine();
@@ -60,11 +80,56 @@ public final class BroBot {
         };
 
         final String command = commandRetriever.get();
+        final Runnable taskAdder = () -> {
+            BroBot.delimit();
+            BroBot.taskList.add(new Task(command));
+            System.out.printf("added: %s\n", command);
+            BroBot.delimit();
+        };
+
         if (command.equals("bye")) {
             BroBot.delimit();
             System.out.println("Bye. Hope to see you again soon!");
             BroBot.delimit();
             return false;
+        }
+
+        if (markCommandPattern.matcher(command).matches()) {
+            final String[] tokens = command.split(" ");
+            final int taskIndex = Integer.parseInt(tokens[1]) - 1;
+            if (0 <= taskIndex && taskIndex < BroBot.taskList.size()) {
+                BroBot.delimit();
+
+                BroBot.taskList.get(taskIndex).mark();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("\t" + BroBot.taskList.get(taskIndex));
+
+                BroBot.delimit();
+
+                return true;
+            } else {
+                taskAdder.run();
+                return true;
+            }
+        } else if (unmarkCommandPattern.matcher(command).matches()) {
+            final String[] tokens = command.split(" ");
+            final int taskIndex = Integer.parseInt(tokens[1]) - 1;
+
+            if (0 <= taskIndex && taskIndex < BroBot.taskList.size()) {
+                BroBot.delimit();
+
+
+                BroBot.taskList.get(taskIndex).unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("\t" + BroBot.taskList.get(taskIndex));
+
+                BroBot.delimit();
+
+                return true;
+            } else {
+                taskAdder.run();
+                return true;
+            }
         }
 
         if (command.equals("list")) {
@@ -76,10 +141,7 @@ public final class BroBot {
             return true;
         }
 
-        BroBot.delimit();
-        BroBot.taskList.add(new Task(command));
-        System.out.printf("added: %s\n", command);
-        BroBot.delimit();
+        taskAdder.run();
         return true;
     }
 
